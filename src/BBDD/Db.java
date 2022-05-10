2ec76df15d2acc626afb25d60929af0493687c53
Juan <j.garrigos91@gmail.com>
2022-05-10 15:52:14 +0200

    Loggin unificado con proyecto. Falta conexion bbdd

--- src/BBDD/Db.java
+++ src/BBDD/Db.java
@@ -2,6 +2,11 @@ package BBDD;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Code.Cliente;
 
 public class Db {
 	
@@ -10,6 +15,8 @@ public class Db {
 	private static String password = "Passw0rd";
 	private static String url="jdbc:oracle:thin:@localhost:1521:"+bd;
 	static Connection connection = null;
+	PreparedStatement pst=null;
+	
 	
 	public static void connect() {
 		
@@ -25,4 +32,47 @@ public class Db {
 		
 	}
 	
+	public boolean insertClientes (Cliente c) {
+		boolean insertado=false;
+
+		try {
+			String sql= "insert into cliente (username, pass) values(?,?)";
+			pst = connection.prepareStatement(sql);
+			pst.setString(1,c.getUsername());
+			pst.setString(2, c.getPass());
+
+			insertado=pst.executeUpdate()>0;        
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return insertado;
+	}
+
+	public Cliente selectCliente (String user) {
+		Cliente cliente=null;
+		ResultSet rs;
+		String username, pass, mail;
+
+		try {
+			String sql="select username, pass, mail from cliente where binary user=?";
+			pst=connection.prepareStatement(sql);
+			pst.setString(1, user);
+			rs=pst.executeQuery();
+
+			if(rs.next()) {
+
+				username=rs.getString("username");
+				pass=rs.getString("pass");
+				mail=rs.getString("mail");
+				cliente=new Cliente(username, pass, mail);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return cliente;
+
+	}
+
+
 }
--- src/Code/Cliente.java
+++ src/Code/Cliente.java
@@ -5,97 +5,156 @@ import java.util.ArrayList;
 
 public class Cliente {
 
+	private String username;
+	private String pass;
+	private String correo;
 	private String nombre;
 	private String apellidos;
 	private String dni;
 	private LocalDate fNacimiento;
 	private int telefono;
-	private String correo;
+	
 	ArrayList<Reserva> listaReservas;
 	
-	public Cliente(String nombre, String apellidos, String dni, LocalDate fNacimiento, int telefono, String correo) {
+	
+
+	public Cliente(String username, String pass, String correo) {
 		super();
-		this.nombre = nombre;
-		this.apellidos = apellidos;
-		this.dni = dni;
-		this.fNacimiento = fNacimiento;
-		this.telefono = telefono;
+		this.username = username;
+		this.pass = pass;
 		this.correo = correo;
 	}
-	
-	public Cliente(String nombre, String apellidos, String dni, String correo) {
-		
-	}
-	
-	public void reservaProducto(Pack p) {
-		
-	}
-	
-	public void reservaPack(Pack p) {
-		
-	}
-	
-	public void confirmarReserva(Reserva r) {
-		
-	}
-	
-	public void anularReserva(Reserva r) {
-		
-	}
-	
-	String getNombre() {
-		return nombre;
-	}
 
-	void setNombre(String nombre) {
+
+	public Cliente(String username, String pass, String correo, String nombre, String apellidos, String dni,
+			LocalDate fNacimiento, int telefono) {
+		super();
+		this.username = username;
+		this.pass = pass;
+		this.correo = correo;
 		this.nombre = nombre;
-	}
-
-	String getApellidos() {
-		return apellidos;
-	}
-
-	void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
-	}
-
-	String getDni() {
-		return dni;
-	}
-
-	void setDni(String dni) {
 		this.dni = dni;
-	}
-
-	LocalDate getfNacimiento() {
-		return fNacimiento;
-	}
-
-	void setfNacimiento(LocalDate fNacimiento) {
 		this.fNacimiento = fNacimiento;
-	}
-
-	int getTelefono() {
-		return telefono;
-	}
-
-	void setTelefono(int telefono) {
 		this.telefono = telefono;
 	}
 
-	String getCorreo() {
+
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+	public String getPass() {
+		return pass;
+	}
+
+
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+
+
+	public String getCorreo() {
 		return correo;
 	}
 
-	void setCorreo(String correo) {
+
+
+	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
 
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+
+
+	public String getDni() {
+		return dni;
+	}
+
+
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+
+
+	public LocalDate getfNacimiento() {
+		return fNacimiento;
+	}
+
+
+
+	public void setfNacimiento(LocalDate fNacimiento) {
+		this.fNacimiento = fNacimiento;
+	}
+
+
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+
+
+	public ArrayList<Reserva> getListaReservas() {
+		return listaReservas;
+	}
+
+
+
+	public void setListaReservas(ArrayList<Reserva> listaReservas) {
+		this.listaReservas = listaReservas;
+	}
+
+
+
 	@Override
 	public String toString() {
-		return "Cliente [nombre=" + nombre + ", apellidos=" + apellidos + ", dni=" + dni + ", fNacimiento="
-				+ fNacimiento + ", telefono=" + telefono + ", correo=" + correo + ", listaReservas=" + listaReservas
-				+ "]";
+		return "Cliente [username=" + username + ", pass=" + pass + ", correo=" + correo + ", nombre=" + nombre
+				+ ", apellidos=" + apellidos + ", dni=" + dni + ", fNacimiento=" + fNacimiento + ", telefono="
+				+ telefono + ", listaReservas=" + listaReservas + "]";
 	}
 	
 	
